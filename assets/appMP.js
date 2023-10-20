cdbf72f6eeec179ed820f98b7dfd9a0fa134f4f4
Federico Giudici <federicogiudici10@gmail.com>
2023-10-20 15:57:41 +0200

    what i made today

--- assets/appMP.js
+++ assets/appMP.js
@@ -1,2 +1,20 @@
 "use strict"
 
+let query = window.matchMedia("(min-width: 650px)");
+let imgAbt = document.getElementById("image-about-container");
+let infAbt = document.getElementById("info-about-container");
+
+function aboutResponsive(){
+    if(query.matches){
+        console.log("ciao");
+        imgAbt.classList.add("basis-half")
+        infAbt.classList.add("basis-half")
+    }
+    else{
+        imgAbt.classList.remove("basis-half")
+        infAbt.classList.remove("basis-half")
+
+    }
+}
+
+query.onchange(aboutResponsive());
